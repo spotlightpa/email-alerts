6ee9c2e4096007bda10e25128f0e22bd5bba3597
Carl Johnson <me@carlmjohnson.net>
2021-08-16 16:02:57 -0400

    Add Validator

--- pkg/emailalerts/validate.go
+++ pkg/emailalerts/validate.go
@@ -1,32 +1,23 @@
 package emailalerts
 
 import (
-	"net/http"
 	"strings"
 
 	"github.com/carlmjohnson/emailx"
-	"github.com/carlmjohnson/resperr"
 )
 
 var forbiddenNames = []string{"://"}
 
 func validate(email, first, last string) error {
-	if email == "" {
-		err := resperr.New(http.StatusBadRequest, "no address")
-		err = resperr.WithUserMessage(err, "No email address provided")
-		return err
-	}
-	if !emailx.Valid(email) {
-		err := resperr.New(http.StatusBadRequest, "invalid email")
-		err = resperr.WithUserMessagef(err,
-			"Invalid email address provided: %q.", email)
-		return err
-	}
+	var v Validator
+	v.Ensure(email != "", "EMAIL", "No email address provided.")
+	v.Ensure(email == "" || emailx.Valid(email), "EMAIL",
+		"Invalid email address provided: %q.", email)
 	for _, s := range forbiddenNames {
-		if strings.Contains(first, s) || strings.Contains(last, s) {
-			return resperr.New(http.StatusBadRequest, "invalid first/last name")
-		}
+		v.Ensure(!strings.Contains(first, s), "FNAME",
+			"First name contains invalid characters %q", s)
+		v.Ensure(!strings.Contains(last, s), "LNAME",
+			"Last name contains invalid characters %q", s)
 	}
-
-	return nil
+	return v.Err()
 }
