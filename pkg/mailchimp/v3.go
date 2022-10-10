0b90d63e945e3d9f97d076bedf79a4f52c7e8c77
Carl Johnson <me@carlmjohnson.net>
2022-10-10 15:06:56 -0400

    Mailchimp: Fix missing error wrap

--- pkg/mailchimp/v3.go
+++ pkg/mailchimp/v3.go
@@ -66,7 +66,7 @@ func (v3 V3) PutUser(ctx context.Context, req *PutUserRequest) error {
 		}
 		if requests.HasStatusErr(err, http.StatusBadRequest) {
 			err = resperr.New(http.StatusBadRequest,
-				"bad address %q", req.EmailAddress)
+				"bad address %q: %w", req.EmailAddress, err)
 		} else {
 			err = resperr.New(http.StatusBadGateway,
 				"problem connecting to MailChimp: %w", err)
