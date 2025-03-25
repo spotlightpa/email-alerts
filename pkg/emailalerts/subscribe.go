45822ca7de8565dc30b4314fecbaeff2becf7734
Carlana <mail@carlana.net>
2025-03-25 19:43:43 -0400

    WIP: Add postSubscribeJSON endpoint

--- pkg/emailalerts/subscribe.go
+++ pkg/emailalerts/subscribe.go
@@ -176,3 +176,108 @@ func (app *appEnv) postSubscribeActiveCampaign(w http.ResponseWriter, r *http.Re
 	dest := validateRedirect(r.FormValue("redirect"), "/thanks.html")
 	http.Redirect(w, r, dest, http.StatusSeeOther)
 }
+
+func (app *appEnv) postSubscribeJSON(w http.ResponseWriter, r *http.Request) http.Handler {
+	app.Printf("start postSubscribeActiveCampaign")
+
+	var req struct {
+		EmailAddress            string `json:"EMAIL"`
+		FirstName               string `json:"FNAME"`
+		LastName                string `json:"LNAME"`
+		Events                  bool   `json:"events"`
+		Investigator            bool   `json:"investigator"`
+		PAPost                  bool   `json:"papost"`
+		BreakingNews            bool   `json:"breaking_news"`
+		PALocal                 bool   `json:"palocal"`
+		BerksCounty             bool   `json:"berks_county"`
+		Berks                   bool   `json:"berks"`         // Alias for BerksCounty
+		TalkOfTheTown           bool   `json:"talkofthetown"` // Alias for StateCollege
+		StateCollege            bool   `json:"state_college"`
+		WeekInReview            bool   `json:"week_in_review"`
+		PennStateAlerts         bool   `json:"pennstatealert"`
+		CentreCountyDocumenters bool   `json:"centre_county_documenters"` // Alias for CentreDocumenters
+		CentreDocumenters       bool   `json:"centredocumenters"`
+		HowWeCare               bool   `json:"howwecare"` // Alias for care
+		Care                    bool   `json:"care"`
+		Honeypot                bool   `json:"contact"`
+		Token                   string `json:"token"`
+	}
+	if err := app.readJSON(r, &req); err != nil {
+		return app.replyErr(err)
+	}
+	if err := validate(req.EmailAddress, req.FirstName, req.LastName); err != nil {
+		return app.replyErr(err)
+	}
+	if !app.verifyToken(time.Now(), req.Token) {
+		return app.replyErr(resperr.New(http.StatusBadRequest, "invalid token %q", req.Token))
+	}
+	if !app.kb.Verify(r.Context(), req.EmailAddress) {
+		err := resperr.New(http.StatusBadRequest,
+			"Kickbox rejected %q", req.EmailAddress)
+		err = resperr.E{E: err,
+			M: "There was a problem with the email address entered. Please check it and try again."}
+		return app.replyErr(err)
+	}
+
+	ip := r.RemoteAddr
+	app.l.Println("looking up IP", ip)
+	ok, err := app.maxcl.IPInCountry(r.Context(), http.DefaultClient, ip,
+		"US", "CA", "UK", "PR")
+	if err != nil {
+		return app.replyErr(err)
+	}
+	if !ok {
+		return app.replyErr(resperr.E{
+			M: "Sorry, due to spam concerns, we are not accept international subscribers at this time."})
+	}
+	app.l.Println("subscribing user", req.EmailAddress)
+
+	interests := map[int]bool{
+		1: true, // Master list
+		3: req.PALocal,
+		4: req.PAPost,
+		5: req.Investigator,
+		6: req.HowWeCare,
+		7: req.TalkOfTheTown ||
+			req.StateCollege,
+		8: req.PennStateAlerts,
+		9: req.BerksCounty ||
+			req.Berks,
+		10: req.BreakingNews,
+		11: req.WeekInReview,
+		13: req.Events,
+	}
+	maps.DeleteFunc(interests, func(k int, v bool) bool {
+		return !v
+	})
+
+	if err := app.ac.CreateContact(r.Context(), activecampaign.Contact{
+		Email:     emailx.Normalize(req.EmailAddress),
+		FirstName: strings.TrimSpace(req.FirstName),
+		LastName:  strings.TrimSpace(req.LastName),
+	}); err != nil {
+		return app.replyErr(err)
+	}
+
+	app.l.Printf("subscribed: email=%q interests=%v", req.EmailAddress, interests)
+	res, err := app.ac.FindContactByEmail(r.Context(), emailx.Normalize(req.EmailAddress))
+	if err != nil {
+		return app.replyErr(err)
+	}
+	if len(res.Contacts) != 1 {
+		err := resperr.New(http.StatusBadRequest,
+			"Could not find user ID %q", req.EmailAddress)
+		err = resperr.E{E: err,
+			M: "There was a problem while processing your request. Please try again."}
+		return app.replyErr(err)
+	}
+	contactID := res.Contacts[0].ID
+	app.l.Printf("found user: id=%d", contactID)
+
+	for _, listID := range slices.Sorted(maps.Keys(interests)) {
+		if err := app.ac.AddToList(r.Context(), listID, contactID); err != nil {
+			return app.replyErr(err)
+		}
+	}
+	return app.replyJSON("ok")
+}
